28d27f5cbc468e15d9652ba67a7f096a9fe1096c
Costas Economou <cgeconomou@hotmail.com>
2022-10-12 04:26:52 +0300

    2nd commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import ME1 from '../../assets/IMGME.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -14,7 +15,7 @@ const Header = () => {
         <CTA/>
         <HeaderSocials/>
         <div className="me">
-          <img src={ME} alt="me" />
+          <img src={ME1} alt="me" />
         </div>
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/hairsallon.png'
+import IMG2 from '../../assets/movies.jpg'
+import IMG3 from '../../assets/rps.png'
+import IMG4 from '../../assets/weather.jpg'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5> My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/cgeconomou" className='btn' target='_blank'>Github</a>
+          {/* <a href="" className='btn btn-primary'>Github</a> */}
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/cgeconomou" className='btn' target='_blank'>Github</a>
+          {/* <a href="" className='btn btn-primary'>Github</a> */}
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/cgeconomou" className='btn' target='_blank'>Github</a>
+          {/* <a href="" className='btn btn-primary'>Github</a> */}
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/cgeconomou" className='btn' target='_blank'>Github</a>
+          {/* <a href="" className='btn btn-primary'>Github</a> */}
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/cgeconomou" className='btn' target='_blank'>Github</a>
+          {/* <a href="" className='btn btn-primary'>Github</a> */}
+        </article>
+      </div>
+    </section>
   )
 }
 
